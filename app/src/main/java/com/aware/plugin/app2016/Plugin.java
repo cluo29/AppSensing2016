51bafe6b811e6c00bccd4c720f04dcb473af0386
cluo29 <mr.cluo29@gmail.com>
2016-04-13 18:59:10 +0300

    Initial commit

---
+++ app/src/main/java/com/aware/plugin/app2016/Plugin.java
@@ -0,0 +1,89 @@
+package com.aware.plugin.app2016;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import com.aware.Aware;
+import com.aware.Aware_Preferences;
+import com.aware.utils.Aware_Plugin;
+import com.aware.plugin.app2016.Provider.Unlock_Monitor_Data;
+
+public class Plugin extends Aware_Plugin {
+
+    public static final String ACTION_AWARE_PLUGIN_APP2016 = "ACTION_AWARE_PLUGIN_ACP_APP2016";
+
+    public static final String EXTRA_DATA = "data";
+
+    //context
+    private static ContextProducer sContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        TAG = "AWARE::"+getResources().getString(R.string.app_name);
+
+        //Activate programmatically any sensors/plugins you need here
+        //e.g., Aware.setSetting(this, Aware_Preferences.STATUS_ACCELEROMETER,true);
+        //NOTE: if using plugin with dashboard, you can specify the sensors you'll use there.
+
+        //Any active plugin/sensor shares its overall context using broadcasts
+        sContext = new ContextProducer() {
+            @Override
+            public void onContext() {
+                //Broadcast your context here
+                ContentValues data = new ContentValues();
+                data.put(Unlock_Monitor_Data.TIMESTAMP, System.currentTimeMillis());
+                data.put(Unlock_Monitor_Data.DEVICE_ID, Aware.getSetting(getApplicationContext(), Aware_Preferences.DEVICE_ID));
+
+                //send to AWARE
+                Intent context_unlock = new Intent();
+                context_unlock.setAction(ACTION_AWARE_PLUGIN_APP2016);
+                context_unlock.putExtra(EXTRA_DATA,data);
+                sendBroadcast(context_unlock);
+
+                getContentResolver().insert(Unlock_Monitor_Data.CONTENT_URI, data);
+            }
+        };
+        CONTEXT_PRODUCER = sContext;
+
+        //Add permissions you need (Support for Android M) e.g.,
+        //REQUIRED_PERMISSIONS.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        //To sync data to the server, you'll need to set this variables from your ContentProvider
+        DATABASE_TABLES = Provider.DATABASE_TABLES;
+        TABLES_FIELDS = Provider.TABLES_FIELDS;
+        //table 1, 2, 3
+        CONTEXT_URIS = new Uri[]{ Provider.Unlock_Monitor_Data.CONTENT_URI};
+
+        /*
+        if (Aware.getSetting(this, "study_id").length() == 0) {
+            Intent joinStudy = new Intent(this, Aware_Preferences.StudyConfig.class);
+            joinStudy.putExtra(Aware_Preferences.StudyConfig.EXTRA_JOIN_STUDY, "https://api.awareframework.com/index.php/webservice/index/634/0FOT21HRz8IZ");
+            startService(joinStudy);
+        }
+        */
+
+        Log.d(TAG, "fuck off");
+    }
+
+
+
+    //This function gets called every 5 minutes by AWARE to make sure this plugin is still running.
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+
+        //Stop plugin
+        Aware.stopPlugin(this, "com.aware.plugin.acpunlock");
+    }
+}
